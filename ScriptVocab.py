c4e221b496b87ff4cb7d282d1861241b3278321b
Kafelixon <piotrszkafel@gmail.com>
2023-06-04 11:20:35 +0200

    Refactored everything into classes

--- ScriptVocab.py
+++ ScriptVocab.py
@@ -1,227 +1,133 @@
 import argparse
 import glob
 import re
-import time
 from collections import Counter
 from pathlib import Path
-
+import time
 import translators as ts
 
 
-def is_time_stamp(line: str) -> bool:
-    """
-    Checks if the given line starts with a timestamp.
+class ScriptVocab:
+    def __init__(self, args):
+        self.path = args["path"]
+        self.subs_language = args["subs_language"]
+        self.target_language = args["target_language"]
+        self.output = args["output"]
+        self.input_extension = args["input_extension"]
+        self.min_word_size = args["min_word_size"]
+        self.min_appearance = args["min_appearance"]
+        self.encoding = args["encoding"]
 
-    A timestamp is defined as a string at the start of the line in the format 'HH:MM:SS,MS', 
-    where HH represents hours, MM represents minutes, SS represents seconds, and MS represents milliseconds.
-
-    Args: line (str): The line to check.
-
-    Returns: bool: True if the line starts with a timestamp, False otherwise.
-    """
-    pattern = r'^\d{1,2}:\d{2}:\d{2},\d{3}'
-    return bool(re.match(pattern, line))
-
-
-def has_letters(line: str) -> bool:
-    """
-    Checks if a line contains any letters.
-
-    Args: line (str): The line to check.
-
-    Returns: bool: True if the line contains any letters, False otherwise.
-    """
-    return bool(re.search('[a-zA-Z]', line))
+        self.filenames = self.create_list_of_files_to_process()
+        self.all_words = []
 
 
-def has_no_text(line: str) -> bool:
-    """
-    Checks if a line is empty, numeric, a timestamp, or contains no letters.
+    def create_list_of_files_to_process(self):
+        path = Path(self.path)
+        if path.is_file():
+            return [path]
+        if path.is_dir():
+            return glob.glob(f"{path}/*.{self.input_extension}")
 
-    Args: line (str): The line to check.
-
-    Returns: bool: True if the line is empty, numeric, a timestamp, or contains no letters. False otherwise.
-    """
-    line = line.strip()
-    return not line or line.isnumeric() or is_time_stamp(line) or not has_letters(line)
-
-
-def is_lowercase_letter_or_comma(char: str) -> bool:
-    """
-    Checks if a character is a lowercase letter or a comma.
-
-    Args: char (str): The character to check.
-
-    Returns: bool: True if the character is a lowercase letter or a comma, False otherwise.
-    """
-    return char.isalpha() and char.lower() == char or char == ','
-
-
-def clean_up(lines: list[str]) -> list[str]:
-    """
-    Cleans up a list of lines by removing non-text lines and combining text broken into multiple lines.
-
-    Args: lines (list): The list of lines to clean up.
-
-    Returns: list: The cleaned up list of lines.
-    """
-    new_lines = []
-    for line in lines[1:]:
-        if has_no_text(line):
-            continue
-        elif new_lines and is_lowercase_letter_or_comma(line[0]):
-            # combine with previous line
-            new_lines[-1] = f"{new_lines[-1].strip()} {line}"
-        else:
-            # append line
-            new_lines.append(re.sub('<.*?>', '', line))
-    return new_lines
-
-
-def translate_chunk(chunk: list[str], input_lang: str, target_lang: str, wait_time: int = 2) -> list[str]:
-    output = ""
-    for attempt in range(2):
-        print(f"Attempt {attempt + 1}...")
-        try:
-            output = ts.translate_text(
-                "\n".join(chunk),
-                translator='bing',
-                from_language=input_lang,
-                to_language=target_lang
-            )
-            break
-        except Exception as e:
-            if attempt < 1:
-                print(
-                    f"Translation failed. Retrying in {wait_time} seconds...")
-                time.sleep(wait_time)
-            else:
-                raise Exception(
-                    f"Translation failed, error: {e}\nTry again later.")
-
-    return output.split("\n")
-
-
-def translate_dictionary(input_dict: dict, input_lang: str, target_lang: str) -> dict[str, str]:
-    """
-    Translates the keys of a dictionary from input_lang to target_lang
-    using a translation service. The translated words become the values in 
-    the returned dictionary.
-
-    Args:
-        input_dict (dict): The dictionary to translate. Only the keys are translated.
-        input_lang (str): The input language (ISO 639-1 code).
-        target_lang (str): The target language (ISO 639-1 code).
-
-    Returns:
-        dict: A dictionary with the original words as keys and translated words as values.
-    """
-    words = list(input_dict.keys())
-    translated_dict = {}
-    chunk_size = 100
-    wait_time = 2
-
-    num_chunks = len(words) // chunk_size + 1
-    for i in range(0, len(words), chunk_size):
-        if i > 0:
-            print(f"Waiting {wait_time} seconds to avoid rate limiting...")
-            time.sleep(wait_time)
-
-        chunk = words[i:i+chunk_size]
-        print(f"Translating chunk {i // chunk_size + 1} of {num_chunks}...")
-
-        translated_words = translate_chunk(
-            chunk, input_lang, target_lang, wait_time)
-        chunk_dict = {key: value for key,
-                      value in zip(chunk, translated_words)}
-        translated_dict.update(chunk_dict)
-
-    return translated_dict
-
-
-def create_dictionary(words: list, min_appearance: int) -> dict[str, int]:
-    """
-    Creates a dictionary from a list of words. Each unique word that appears 
-    at least a certain number of times in the list becomes a key in the dictionary. 
-    The values are the counts of each word. The dictionary is sorted in descending order 
-    by count.
-
-    Args:
-        words (list): The list of words to count.
-        min_appearance (int): The minimum number of appearances for a word to be included 
-        in the dictionary.
-
-    Returns:
-        dict: A dictionary with words as keys and counts as values, sorted 
-        in descending order by count.
-    """
-    # Count word occurrences
-    word_counts: dict[str, int] = Counter(words)
-
-    # Filter words that appear at least min_appearance times
-    word_counts = {
-        word: count
-        for word, count in word_counts.items()
-        if count >= min_appearance
-    }
-
-    # Return sorted dictionary by count in descending order
-    return dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
-
-
-def create_word_list_from_text(text: str, min_word_size: int) -> list[str]:
-    """
-    Extracts words from the given text and returns them as a list.
-
-    Words are defined as sequences of alphanumeric characters. Non-word 
-    lines (e.g. timestamps, lines with no alphabetic characters) are ignored.
-
-    Args: text (str): The text to extract words from.
-
-    Returns: list[str]: The list of words extracted from the text.
-    """
-    # Use list comprehension to generate the word list directly
-    return [word for word in re.findall(r'\b\w+\b', text) if not has_no_text(word) and len(word) > min_word_size]
-
-
-def make_output_filename(filename: str, output_extension: str) -> str:
-    """
-    Creates an output filename by removing the original extension and 
-    appending a new one.
-
-    Args: filename (str): The original filename. output_extension (str): The new extension to append.
-
-    Returns: str: The output filename.
-    """
-    # Strip the extension, add the prefix and the new extension
-    return f"Dictionary [{filename.rsplit('.', 1)[0]}].{output_extension}"
-
-
-def create_list_of_files_to_process(input_path: str, input_extension: str) -> list[str]:
-    path = Path(input_path)
-
-    # Create a list of files to process
-    if path.is_file():
-        filenames = [path]
-    elif path.is_dir():
-        filenames = glob.glob(f"{path}/*.{input_extension}")
-    else:
-        print(
+        raise FileNotFoundError(
             f"Error: The path {path} does not exist or is not a file or directory.")
-        return
-    return filenames
+
+    def process_files(self):
+        print(f"Processing {len(self.filenames)} files...")
+        for filename in self.filenames:
+            with open(filename, encoding=self.encoding, errors='replace') as f:
+                lines = f.readlines()
+            text = ' '.join(self.clean_up(lines)).lower()
+            print(text)
+            words = self.create_word_list_from_text(text, self.min_word_size)
+            print(f"Found {len(words)} words in {filename}")
+            self.all_words.extend(words)
+
+    def has_no_text(self, line):
+        line = line.strip()
+        is_empty_or_numeric = not line or line.isnumeric()
+        matches_timestamp = bool(re.match(r'^\d{1,2}:\d{2}:\d{2},\d{3}', line))
+        contains_no_letters = not bool(re.search('[a-zA-Z]', line))
+        return is_empty_or_numeric or matches_timestamp or contains_no_letters
+
+    def is_lowercase_letter_or_comma(self, char: str):
+        return char.isalpha() and char.lower() == char or char == ','
+
+    def clean_up(self, lines):
+        new_lines = [re.sub('<.*?>', '', line)
+                     for line in lines[1:] if not self.has_no_text(line)]
+        for i in range(1, len(new_lines)):
+            if self.is_lowercase_letter_or_comma(new_lines[i][0]):
+                new_lines[i-1] += ' ' + new_lines.pop(i)
+        return new_lines
+
+    def create_word_list_from_text(self, text, min_word_size):
+        return [word for word in re.findall(r'\b\w+\b', text) if not self.has_no_text(word) and len(word) > min_word_size]
+
+    def translate_chunk(self, chunk, input_lang, target_lang, wait_time=2):
+        for attempt in range(2):
+            print(f"Attempt {attempt + 1}...")
+            try:
+                return ts.translate_text("\n".join(chunk),
+                                         translator='bing',
+                                         from_language=input_lang,
+                                         to_language=target_lang).split("\n")
+            except Exception as e:
+                if attempt < 1:
+                    print(
+                        f"Translation failed. Retrying in {wait_time} seconds...")
+                    time.sleep(wait_time)
+                else:
+                    raise Exception(
+                        f"Translation failed, error: {e}\nTry again later.")
+
+    def translate_dictionary(self, dictionary, source_language, target_language):
+        words = list(dictionary.keys())
+        translated_dict = {}
+        chunk_size = 100
+        wait_time = 2
+
+        num_chunks = len(words) // chunk_size + 1
+        for i in range(0, len(words), chunk_size):
+            if i > 0:
+                print(f"Waiting {wait_time} seconds to avoid rate limiting...")
+                time.sleep(wait_time)
+            chunk = words[i:i+chunk_size]
+            print(
+                f"Translating chunk {i // chunk_size + 1} of {num_chunks}...")
+            translated_words = self.translate_chunk(
+                chunk, source_language, target_language, wait_time)
+            translated_dict.update(dict(zip(chunk, translated_words)))
+
+        return translated_dict
+
+    def create_dictionary(self, words, min_appearance):
+        word_count = Counter(words)
+        word_counts = {word: count for word,
+                       count in word_count.items() if count >= min_appearance}
+        return dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
+
+    def run(self):
+        self.process_files()
+        print(f"Found {len(self.all_words)} words in total.")
+        words_dict = self.create_dictionary(
+            self.all_words, self.min_appearance)
+        print(
+            f"Found {len(words_dict)} unique words appearing at least {self.min_appearance} times.")
+
+        translated_dict = self.translate_dictionary(
+            words_dict, self.subs_language, self.target_language)
+
+        with open(self.output, 'w') as f:
+            f.write("Count, Word, Translation\n")
+            for word, count in words_dict.items():
+                translation = translated_dict.get(word, '')
+                f.write(f"{count}, {word}, {translation}\n")
 
 
 def main():
-    """
-    The main function to clean up a text files, count the words, translate them, 
-    and write the results to a new file.
-    """
-
-    # Initialize the ArgumentParser object
     parser = argparse.ArgumentParser()
 
-    # Add arguments
     parser.add_argument(
         "path", help="The path to the text file or directory to process.")
     parser.add_argument("-s", "--subs_language", default='auto',
@@ -239,48 +145,9 @@ def main():
     parser.add_argument("-e", "--encoding", default='utf-8',
                         help="The encoding of the text file. Default is 'utf-8'. If you see a lot of [?]s replacing characters, try 'cp1252'.")
 
-    # Parse the arguments
     args = parser.parse_args()
-    filenames = create_list_of_files_to_process(
-        args.path, args.input_extension)
-
-    print(ts.translators_pool())
-
-    # Process each file in the list to create a dictionary of words
-    all_words = process_files(
-        args.encoding, args.min_word_size, filenames)
-    print(f"Found {len(all_words)} words in total.")
-
-    words_dict = create_dictionary(all_words, args.min_appearance)
-    print(
-        f"Found {len(words_dict)} unique words appearing at least {args.min_appearance} times.")
-
-    # Translate the words
-    translated_dict = translate_dictionary(
-        words_dict, args.subs_language, args.target_language)
-
-    # Write the results to a file
-    with open(args.output, 'w') as f:
-        f.write("Count, Word, Translation\n")
-        for word in words_dict:
-            count = str(words_dict[word])
-            translation: str = translated_dict.get(word, '')
-            f.write(f"{count}, {word}, {translation}\n")
-
-
-def process_files(encoding, min_word_size, filenames) -> list[str]:
-    all_words = []
-    print(f"Processing {len(filenames)} files...")
-    for filename in filenames:
-        with open(filename, encoding=encoding, errors='replace') as f:
-            lines: list[str] = f.readlines()
-
-        text: str = ' '.join(clean_up(lines)).lower()
-        words = create_word_list_from_text(text, min_word_size)
-        print(f"Found {len(words)} words in {filename}")
-        all_words.extend(words)
-
-    return all_words
+    translator = ScriptVocab(dict(args._get_kwargs()))
+    translator.run()
 
 
 if __name__ == '__main__':
--- test_ScriptVocab.py
+++ test_ScriptVocab.py
@@ -1,96 +1,154 @@
-import unittest
-from ScriptVocab import *
-from unittest.mock import patch
+import os
 import time
+import unittest
+from pathlib import Path
+from unittest.mock import MagicMock, Mock, patch
+
+from ScriptVocab import ScriptVocab
 
 
 class TestScriptVocab(unittest.TestCase):
 
-    def test_is_time_stamp_with_timestamp(self):
-        self.assertTrue(is_time_stamp('00:01:23,456'))
+    def setUp(self):
+        os.makedirs('/tmp/dir', exist_ok=True)
+        Path('/tmp/dir/file.txt').touch()
+        Path('/tmp/dir/file2.txt').touch()
+        Path('/tmp/file.txt').touch()
 
-    def test_is_time_stamp_with_no_timestamp(self):
-        self.assertFalse(is_time_stamp('This is not a timestamp'))
+        args = {'path': '/tmp/file.txt', 'subs_language': 'auto', 'target_language': 'en', 'output': 'output.txt',
+                'input_extension': 'txt', 'min_word_size': 1, 'min_appearance': 4, 'encoding': 'utf-8'}
+        self.sv = ScriptVocab(args)
 
-    def test_has_letters_with_letters(self):
-        self.assertTrue(has_letters('This line has letters'))
+    def tearDown(self):
+        Path('/tmp/dir/file.txt').unlink(missing_ok=True)
+        Path('/tmp/dir/file2.txt').unlink(missing_ok=True)
+        Path('/tmp/file.txt').unlink(missing_ok=True)
+        os.rmdir('/tmp/dir')
 
-    def test_has_letters_with_numbers_and_no_letters(self):
-        self.assertFalse(has_letters('1234567890'))
+    def test_create_list_of_files_to_process_with_file(self):
+        self.sv.path = '/tmp/file.txt'
+        self.assertEqual(self.sv.create_list_of_files_to_process(), [
+                         Path('/tmp/file.txt')])
 
-    def test_has_letters_with_punctuation_and_no_letters(self):
-        self.assertFalse(has_letters('!!!,,,,...;;;'))
+    def test_create_list_of_files_to_process_with_directory(self):
+        self.sv.path = '/tmp/dir'
+        self.assertEqual(
+            set(self.sv.create_list_of_files_to_process()),
+            set([str(Path('/tmp/dir/file.txt')), str(Path('/tmp/dir/file2.txt'))]))
+
+    def test_create_list_of_files_to_process_with_nonexistent_path(self):
+        self.sv.path = '/nonexistent/path'
+        with self.assertRaises(FileNotFoundError):
+            self.sv.create_list_of_files_to_process()
 
     def test_has_no_text_with_letters(self):
-        self.assertFalse(has_no_text('This line has letters'))
+        self.assertFalse(self.sv.has_no_text('This line has letters'))
 
     def test_has_no_text_with_numbers(self):
-        self.assertTrue(has_no_text('1234567890'))
+        self.assertTrue(self.sv.has_no_text('1234567890'))
 
     def test_has_no_text_with_punctuation(self):
-        self.assertTrue(has_no_text('!!!,,,,...;;;'))
+        self.assertTrue(self.sv.has_no_text('!!!,,,,...;;;'))
 
     def test_has_no_text_with_letters_and_numbers(self):
-        self.assertFalse(has_no_text(
+        self.assertFalse(self.sv.has_no_text(
             'This line has letters and numbers 1234567890'))
 
     def test_has_no_text_with_letters_and_punctuation(self):
-        self.assertFalse(has_no_text(
+        self.assertFalse(self.sv.has_no_text(
             'This line has letters and punctuation !!!,,,,...;;;'))
 
     def test_has_no_text_with_time_stamp(self):
-        self.assertTrue(has_no_text('00:01:23,456'))
+        self.assertTrue(self.sv.has_no_text('00:01:23,456'))
 
     def test_has_no_text_empty_string(self):
-        self.assertTrue(has_no_text(''))
+        self.assertTrue(self.sv.has_no_text(''))
 
     def test_is_lowercase_letter_or_comma_lowercase(self):
-        self.assertTrue(is_lowercase_letter_or_comma('a'))
+        self.assertTrue(self.sv.is_lowercase_letter_or_comma('a'))
 
     def test_is_lowercase_letter_or_comma_comma(self):
-        self.assertTrue(is_lowercase_letter_or_comma(','))
+        self.assertTrue(self.sv.is_lowercase_letter_or_comma(','))
 
     def test_is_lowercase_letter_or_comma_uppercase(self):
-        self.assertFalse(is_lowercase_letter_or_comma('A'))
+        self.assertFalse(self.sv.is_lowercase_letter_or_comma('A'))
 
     def test_is_lowercase_letter_or_comma_number(self):
-        self.assertFalse(is_lowercase_letter_or_comma('1'))
+        self.assertFalse(self.sv.is_lowercase_letter_or_comma('1'))
 
     def test_is_lowercase_letter_or_comma_punctuation(self):
-        self.assertFalse(is_lowercase_letter_or_comma('!'))
+        self.assertFalse(self.sv.is_lowercase_letter_or_comma('!'))
 
     def test_clean_up(self):
-        lines = ['00:01:23,456', '', 'This line should remain']
-        expected = ['This line should remain']
-        self.assertEqual(clean_up(lines), expected)
+        lines = ['00:01:23,456', '',
+                 'This line should remain', 'Hello with number 1']
+        expected = ['This line should remain', 'Hello with number 1']
+        self.assertEqual(self.sv.clean_up(lines), expected)
 
     def test_create_word_list_from_text_min_word_size(self):
         text = "This is a simple test text"
         expected = ['This', 'simple', 'test', 'text']
-        self.assertEqual(create_word_list_from_text(text, 3), expected)
+        self.assertEqual(self.sv.create_word_list_from_text(text, 3), expected)
 
     def test_create_word_list_from_text_with_no_text(self):
         text = "1234 1234567890"
         expected = []
-        self.assertEqual(create_word_list_from_text(text, 1), expected)
+        self.assertEqual(self.sv.create_word_list_from_text(text, 1), expected)
 
     def test_create_word_list_from_text_min_word_size_with_no_text(self):
         text = "word 1234 1234567890 w1rd sad"
         expected = ['word', 'w1rd']
-        self.assertEqual(create_word_list_from_text(text, 3), expected)
+        self.assertEqual(self.sv.create_word_list_from_text(text, 3), expected)
 
     def test_create_dictionary(self):
         words = ['test', 'test', 'example', 'example', 'example']
         expected = {'example': 3, 'test': 2}
-        self.assertEqual(create_dictionary(words, 2), expected)
+        self.assertEqual(self.sv.create_dictionary(words, 2), expected)
 
     def test_create_dictionary_with_word_to_filter_out(self):
         words = ['test', 'test', 'example', 'example', 'example', 'filterme']
         expected = {'example': 3, 'test': 2}
-        self.assertEqual(create_dictionary(words, 2), expected)
+        self.assertEqual(self.sv.create_dictionary(words, 2), expected)
 
-    @patch('ScriptVocab.ts.translate_text')  # Patch the function
-    # Patch the sleep function to speed up tests
+    @patch('ScriptVocab.ts.translate_text')
+    @patch('ScriptVocab.time.sleep')
+    def test_translate_chunk(self, sleep_mock, mock_translate_text):
+        mock_translate_text.return_value = "translated text"
+
+        chunk = ['text1', 'text2']
+        input_lang = 'es'
+        target_lang = 'en'
+
+        translated = self.sv.translate_chunk(chunk, input_lang, target_lang)
+        mock_translate_text.assert_called_with(
+            "\n".join(chunk),
+            translator='bing',
+            from_language=input_lang,
+            to_language=target_lang
+        )
+        self.assertEqual(translated, ['translated text'])
+        self.assertEqual(sleep_mock.call_count, 0)
+
+    @patch('ScriptVocab.ts.translate_text')
+    @patch('ScriptVocab.time.sleep')
+    def test_translate_chunk_with_exception(self, sleep_mock, mock_translate_text):
+        mock_translate_text.return_value = "translated text"
+
+        chunk = ['text1', 'text2']
+        input_lang = 'es'
+        target_lang = 'en'
+
+        translated = self.sv.translate_chunk(chunk, input_lang, target_lang)
+        mock_translate_text.assert_called_with(
+            "\n".join(chunk),
+            translator='bing',
+            from_language=input_lang,
+            to_language=target_lang
+        )
+        self.assertEqual(translated, ['translated text'])
+        self.assertEqual(sleep_mock.call_count, 1)
+
+    @patch('ScriptVocab.ts.translate_text')
     @patch('ScriptVocab.time.sleep')
     def test_translate_dictionary(self, sleep_mock, translate_mock):
         # Setup mock to return specific translation
@@ -102,7 +160,7 @@ class TestScriptVocab(unittest.TestCase):
         target_lang = 'en'
 
         # Call the function - translate_text has been replaced by the mock
-        result = translate_dictionary(
+        result = self.sv.translate_dictionary(
             test_dict, input_lang, target_lang)
 
         # Check that translate_text was called with the correct arguments
@@ -117,25 +175,30 @@ class TestScriptVocab(unittest.TestCase):
         expected_result = {"palabra1": "word1",
                            "palabra2": "word2", "palabra3": "word3"}
         self.assertEqual(result, expected_result)
+        # Check that the sleep function was not called
+        self.assertEqual(sleep_mock.call_count, 0)
 
     @patch('ScriptVocab.ts.translate_text')
     @patch('ScriptVocab.time.sleep')
     def test_translate_dictionary_with_exception(self, sleep_mock, translate_mock):
         # Simulate an exception on the first call and success on the second
-        translate_mock.side_effect = [Exception('Test exception'), "word1\nword2\nword3"]
+        translate_mock.side_effect = [
+            Exception('Test exception'), "word1\nword2\nword3"]
 
         test_dict = {"palabra1": 5, "palabra2": 3, "palabra3": 1}
         input_lang = 'es'
         target_lang = 'en'
-        result = translate_dictionary(test_dict, input_lang, target_lang)
+        result = self.sv.translate_dictionary(
+            test_dict, input_lang, target_lang)
 
         # The translate_text method should have been called twice because of the exception
         self.assertEqual(translate_mock.call_count, 2)
 
-        # The sleep function should have been called once before the second attempt
-        sleep_mock.assert_called_once_with(2)
+        # Check that the sleep function was called once before the second attempt
+        self.assertEqual(sleep_mock.call_count, 1)
 
-        expected_result = {"palabra1": "word1", "palabra2": "word2", "palabra3": "word3"}
+        expected_result = {"palabra1": "word1",
+                           "palabra2": "word2", "palabra3": "word3"}
         self.assertEqual(result, expected_result)
 
     @patch('ScriptVocab.ts.translate_text')
@@ -149,27 +212,13 @@ class TestScriptVocab(unittest.TestCase):
         target_lang = 'en'
 
         with self.assertRaises(Exception):
-            translate_dictionary(test_dict, input_lang, target_lang)
+            self.sv.translate_dictionary(test_dict, input_lang, target_lang)
 
         # The translate_text method should have been called twice because of the exception
         self.assertEqual(translate_mock.call_count, 2)
 
-        # The sleep function should have been called once before the second attempt
-        sleep_mock.assert_called_once_with(2)
-
-    def test_make_output_filename(self):
-        filename = 'input.txt'
-        output_extension = 'csv'
-        expected = 'Dictionary [input].csv'
-        self.assertEqual(make_output_filename(
-            filename, output_extension), expected)
-
-    def test_make_output_filename_with_extension(self):
-        filename = 'input.subtitle.txt'
-        output_extension = 'csv'
-        expected = 'Dictionary [input.subtitle].csv'
-        self.assertEqual(make_output_filename(
-            filename, output_extension), expected)
+        # Check that the sleep function was called twice before the second attempt
+        self.assertEqual(sleep_mock.call_count, 1)
 
 
 if __name__ == '__main__':
