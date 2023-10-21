2d820356c297882c44ac0ff18fa8ea7459cf2e0e
Kafelixon <piotrszkafel@gmail.com>
2023-10-21 10:43:33 +0200

    Added docstrings to script vocab class

--- script_vocab/script_vocab.py
+++ script_vocab/script_vocab.py
@@ -1,3 +1,18 @@
+"""
+This module defines the ScriptVocab and ScriptVocabConfig classes 
+for processing text files and creating a vocabulary of words.
+
+Classes:
+    ScriptVocab: A class for processing text files and creating a vocabulary of words.
+    ScriptVocabConfig: A configuration object for the ScriptVocab class.
+
+Constants:
+    CHUNK_SIZE (int): The size of the chunks used for translating text.
+
+Exceptions:
+    FileNotFoundError: Raised when the input path does not exist or is not a file or directory.
+"""
+
 import glob
 import re
 import socket
@@ -9,6 +24,20 @@ CHUNK_SIZE = 100
 
 
 class ScriptVocabConfig:
+    """
+    A configuration object for the ScriptVocab class.
+
+    Attributes:
+        subs_language (str): Defaults to "auto".
+            The language of the subtitles. 
+        target_language (str): Defaults to "en".
+            The target language for vocabulary extraction. 
+        min_word_size (int): Defaults to 1.
+            The minimum length of a word to be included in the vocabulary. 
+        min_appearance (int): Defaults to 4.
+            The minimum number of times a word must appear to be included in the vocabulary.
+    """
+
     def __init__(
         self,
         subs_language="auto",
@@ -23,19 +52,81 @@ class ScriptVocabConfig:
 
 
 class ScriptVocab:
+    """
+    A class for processing text files and creating a vocabulary of words.
+
+    Attributes:
+        config (ScriptVocabConfig): An instance of ScriptVocabConfig class.
+        all_words (list[str]): A list of all words found in the input text.
+        output (list[str]): A list of translated words.
+        _translators_imported (bool): A flag to check if the translators module is imported.
+
+    Methods:
+        __init__(config: ScriptVocabConfig): Initializes the ScriptVocab class.
+        __enter__(self): Returns the instance of the ScriptVocab class.
+        __exit__(exc_type, exc_value, traceback): Prints "Exiting".
+
+        is_internet_available(self) -> bool: 
+            Checks if the internet is available.
+        input_text(text: str): 
+            Processes input text and adds words to all_words.
+        input_files(path: str, input_extension: str, encoding: str): 
+            Processes input files and adds words to all_words.
+        process_files(file_paths: list[str], encoding: str): 
+            Processes a list of file paths and adds words to all_words.
+        has_no_text(line: str) -> bool: 
+            Checks if a line has no text.
+        is_lowercase_letter_or_comma(char: str) -> bool: 
+            Checks if a character is a lowercase letter or comma.
+        clean_up(lines: list[str]) -> list[str]: 
+            Cleans up lines of text.
+        create_word_list_from_text(text: str, min_word_size: int) -> list[str]: 
+            Creates a list of words from text.
+        translate_chunk(chunk: list[str], input_lang: str, target_lang: str, wait_time: int = 2
+            ) -> list[str]: Translates a chunk of text.
+        translate_text(chunk: list[str], input_lang: str, target_lang: str) -> list[str]: 
+            Translates text using the translators module.
+        convert_to_chunks(words_list: list[str], chunk_size: int) -> list[list[str]]: 
+            Converts a list of words into chunks.
+        translate_dictionary(
+            dictionary: dict[str, int],
+            source_language: str,
+            target_language: str,
+            chunk_size: int,
+            ) -> dict[str, str]: Translates a dictionary of words.
+    """
+
     def __init__(self, config: ScriptVocabConfig):
+        """
+        Initializes the ScriptVocab class.
+
+        Args:
+            config (ScriptVocabConfig): An instance of ScriptVocabConfig class.
+        """
         self.config = config
         self.all_words: list[str] = []
         self.output: list[str] = []
         self._translators_imported: bool = False
 
     def __enter__(self):
+        """
+        Returns the instance of the ScriptVocab class.
+        """
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Prints "Exiting".
+        """
         print("Exiting")
 
-    def is_internet_available(self):
+    def is_internet_available(self) -> bool:
+        """
+        Checks if the internet is available.
+
+        Returns:
+            bool: True if the internet is available, False otherwise.
+        """
         try:
             sock = socket.create_connection(("www.google.com", 80))
             if sock is not None:
@@ -47,11 +138,25 @@ class ScriptVocab:
         return False
 
     def input_text(self, text: str):
+        """
+        Processes input text and adds words to all_words.
+
+        Args:
+            text (str): The input text to process.
+        """
         words = self.create_word_list_from_text(text, self.config.min_word_size)
         print(f"Found {len(words)} words in input text.")
         self.all_words.extend(words)
 
     def input_files(self, path: str, input_extension: str, encoding: str):
+        """
+        Processes input files and adds words to all_words.
+
+        Args:
+            path (str): The path to the input files.
+            input_extension (str): The extension of the input files.
+            encoding (str): The encoding of the input files.
+        """
         path_obj = Path(path)
         if path_obj.is_file():
             filenames = [path_obj]
@@ -63,7 +168,14 @@ class ScriptVocab:
             )
         self.process_files(filenames, encoding)
 
-    def process_files(self, file_paths, encoding):
+    def process_files(self, file_paths: list[str], encoding: str):
+        """
+        Processes a list of file paths and adds words to all_words.
+
+        Args:
+            file_paths (list[str]): A list of file paths to process.
+            encoding (str): The encoding of the input files.
+        """
         for file_path in file_paths:
             with open(file_path, "r", encoding=encoding) as f:
                 lines = f.readlines()
@@ -74,17 +186,44 @@ class ScriptVocab:
                     )
                     self.all_words.extend(words)
 
-    def has_no_text(self, line) -> bool:
+    def has_no_text(self, line: str) -> bool:
+        """
+        Checks if a line has no text.
+
+        Args:
+            line (str): The line to check.
+
+        Returns:
+            bool: True if the line has no text, False otherwise.
+        """
         line = line.strip()
         is_empty_or_numeric = not line or line.isnumeric()
         matches_timestamp = bool(re.match(r"^\d{1,2}:\d{2}:\d{2},\d{3}", line))
         contains_no_letters = not bool(re.search("[a-zA-Z]", line))
         return is_empty_or_numeric or matches_timestamp or contains_no_letters
 
-    def is_lowercase_letter_or_comma(self, char: str):
+    def is_lowercase_letter_or_comma(self, char: str) -> bool:
+        """
+        Checks if a character is a lowercase letter or comma.
+
+        Args:
+            char (str): The character to check.
+
+        Returns:
+            bool: True if the character is a lowercase letter or comma, False otherwise.
+        """
         return char.isalpha() and char.lower() == char or char == ","
 
-    def clean_up(self, lines):
+    def clean_up(self, lines: list[str]) -> list[str]:
+        """
+        Cleans up lines of text.
+
+        Args:
+            lines (list[str]): A list of lines to clean up.
+
+        Returns:
+            list[str]: A list of cleaned up lines.
+        """
         cleaner_lines = [
             re.sub("<.*?>", "", line).rstrip()
             for line in lines
@@ -95,14 +234,38 @@ class ScriptVocab:
                 cleaner_lines[i - 1] += " " + cleaner_lines.pop(i)
         return cleaner_lines
 
-    def create_word_list_from_text(self, text, min_word_size):
+    def create_word_list_from_text(self, text: str, min_word_size: int) -> list[str]:
+        """
+        Creates a list of words from text.
+
+        Args:
+            text (str): The text to create the list of words from.
+            min_word_size (int): The minimum size of a word.
+
+        Returns:
+            list[str]: A list of words.
+        """
         return [
             word.lower()
             for word in re.findall(r"\b\w+\b", text)
             if not self.has_no_text(word) and len(word) >= min_word_size
         ]
 
-    def translate_chunk(self, chunk, input_lang, target_lang, wait_time=2) -> list[str]:
+    def translate_chunk(
+        self, chunk: list[str], input_lang: str, target_lang: str, wait_time: int = 2
+    ) -> list[str]:
+        """
+        Translates a chunk of text.
+
+        Args:
+            chunk (list[str]): The chunk of text to translate.
+            input_lang (str): The input language.
+            target_lang (str): The target language.
+            wait_time (int): The time to wait before retrying the translation.
+
+        Returns:
+            list[str]: A list of translated words.
+        """
         translated_chunk = []
         for attempt in range(2):
             try:
@@ -118,7 +281,20 @@ class ScriptVocab:
                     ) from e
         return translated_chunk
 
-    def translate_text(self, chunk: list, input_lang, target_lang):
+    def translate_text(
+        self, chunk: list[str], input_lang: str, target_lang: str
+    ) -> list[str]:
+        """
+        Translates text using the translators module.
+
+        Args:
+            chunk (list[str]): The chunk of text to translate.
+            input_lang (str): The input language.
+            target_lang (str): The target language.
+
+        Returns:
+            list[str]: A list of translated words.
+        """
         translated_chunk = []
         if self.is_internet_available():
             if not self._translators_imported:
@@ -131,7 +307,6 @@ class ScriptVocab:
                     print("Failed to import translators module.")
             if self._translators_imported:
                 try:
-                    print("here")
                     translated_chunk = str(
                         ts.translate_text(
                             "\n".join(chunk),
@@ -142,20 +317,61 @@ class ScriptVocab:
                     ).split("\n")
                     return translated_chunk
                 except Exception as e:
-                    raise ScriptVocab.ExternalTranslationError("Translation failed") from e
+                    raise ScriptVocab.ExternalTranslationError(
+                        "Translation failed"
+                    ) from e
         print("You are offline, using offline translation")
         for _ in chunk:
             translated_chunk.append("placeholder")
         return translated_chunk
 
-    def convert_to_chunks(self, words_list: list[str], chunk_size: int) -> list[list[str]]:
-        return [words_list[i : i + chunk_size] for i in range(0, len(words_list), chunk_size)]
+    def convert_to_chunks(
+        self, words_list: list[str], chunk_size: int
+    ) -> list[list[str]]:
+        """
+        Converts a list of words into chunks.
+
+        Args:
+            words_list (list[str]): The list of words to convert.
+            chunk_size (int): The size of each chunk.
+
+        Returns:
+            list[list[str]]: A list of chunks.
+        """
+        return [
+            words_list[i : i + chunk_size]
+            for i in range(0, len(words_list), chunk_size)
+        ]
 
     def translate_dictionary(
-        self, dictionary: dict[str, int], source_language, target_language, chunk_size
-    ):
+        self,
+        dictionary: dict[str, int],
+        source_language: str,
+        target_language: str,
+        chunk_size: int,
+    ) -> dict[str, str]:
+        """
+        Translates a dictionary of words.
+
+        Args:
+            dictionary (dict[str, int]): The dictionary of words to translate.
+            source_language (str): The source language.
+            target_language (str): The target language.
+            chunk_size (int): The size of each chunk.
+
+        Returns:
+            dict[str, str]: A dictionary of translated words.
+        """
         translated_dict: dict[str, str] = {}
         chunks = self.convert_to_chunks(list(dictionary.keys()), chunk_size)
+        # for chunk in chunks:
+        #     translated_chunk = self.translate_chunk(
+        #         chunk, source_language, target_language
+        #     )
+        #     for index, value in enumerate(chunk):
+        #         translated_dict[value] = translated_chunk[index]
+        # return translated_dict
+
         for chunk in chunks:
             print(f"Translating chunk {chunks.index(chunk) + 1}")
             translated_words = self.translate_chunk(
@@ -166,6 +382,18 @@ class ScriptVocab:
         return translated_dict
 
     def create_dictionary(self, words, min_appearance):
+        """
+        Create a dictionary of words and their counts from a list of words.
+
+        Args:
+            words (list): 
+                A list of words to count.
+            min_appearance (int): 
+                The minimum number of times a word must appear to be included in the dictionary.
+
+        Returns:
+            dict: A dictionary of words and their counts, sorted by count in descending order.
+        """
         word_count = Counter(words)
         word_counts = {
             word: count for word, count in word_count.items() if count >= min_appearance
@@ -173,6 +401,10 @@ class ScriptVocab:
         return dict(sorted(word_counts.items(), key=lambda item: item[1], reverse=True))
 
     def run(self):
+        """
+            Runs the script, creating a dictionary of words and their counts, 
+            translating the dictionary, and appending the results to the output list.
+            """
         words_dict: dict[str, int] = self.create_dictionary(
             self.all_words, self.config.min_appearance
         )
@@ -190,6 +422,15 @@ class ScriptVocab:
             self.output.append(f"{count}, {word}, {translation}")
 
     def save_output_to_file(self, output_file):
+        """
+        Saves the output of the script to a file.
+
+        Args:
+            output_file (str): The path to the output file.
+
+        Returns:
+            None
+        """
         with open(output_file, "w", encoding="UTF-8") as f:
             if len(self.output) > 0:
                 f.write("Count, Word, Translation\n")
@@ -198,6 +439,16 @@ class ScriptVocab:
         print(f"Output saved to {output_file}")
 
     def get_output_as_json(self):
+        """
+        Converts the output of the ScriptVocab program to a dictionary in JSON format.
+
+        Returns:
+            A list of entries, where each entry represents a.
+            Each entry has the following keys:
+                - "occurrences": the number of times the word appears in the input file
+                - "original_text": the original word in the input file
+                - "translated_text": the translated word in the output file
+        """
         response = []
         for line in self.output:
             line_items = line.split(",")
@@ -218,4 +469,6 @@ class ScriptVocab:
         return response
 
     class ExternalTranslationError(Exception):
-        pass
+        """
+        An exception to handle external translation errors.
+        """
